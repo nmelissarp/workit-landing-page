210bb5908c5014dc2f036cb49314a3113109bc43
nmelissarp <nmelissarp077@gmail.com>
2024-12-19 22:25:08 -0600

    deploying my app

--- src/app/components/Cta/index.tsx
+++ src/app/components/Cta/index.tsx
@@ -13,7 +13,7 @@ const Cta = () => (
     </div>
     <div className={styles.ctaContainer__content}>
       <h2>Be the first to test</h2>
-      <p>Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our app and kickstart your company. Apply for access below and I’ll be in touch to schedule a call.</p>
+      <p>Hi, I&aposm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our app and kickstart your company. Apply for access below and I’ll be in touch to schedule a call.</p>
       <button>Apply for access</button>
     </div>
     <Image 
